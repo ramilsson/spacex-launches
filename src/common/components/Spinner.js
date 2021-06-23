9899375e8fd94a050a2bbfb34932ff37c9183f4c
Ruslan Khuzhin <khuzh.n@gmail.com>
2021-06-23 09:44:46 +0500

    add Spinner component

---
+++ src/common/components/Spinner.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "common/components/Spinner.scss";
+
+export function Spinner() {
+  return (
+    <div className="spinner">
+      <div className="spinner__icon" />
+    </div>
+  );
+}
